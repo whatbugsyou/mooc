1d8edb4ade3c42db502abd35554f35260795d5f9
whatbugyou <1050466416@qq.com>
2019-08-07 14:03:31 +0800

    补充漏掉的dao层mapper文件

---
+++ src/com/mooc/dao/CoursetypeMapper.java
@@ -0,0 +1,30 @@
+package com.mooc.dao;
+
+import com.mooc.entity.Coursetype;
+import com.mooc.entity.CoursetypeExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CoursetypeMapper {
+    int countByExample(CoursetypeExample example);
+
+    int deleteByExample(CoursetypeExample example);
+
+    int deleteByPrimaryKey(Integer ctid);
+
+    int insert(Coursetype record);
+
+    int insertSelective(Coursetype record);
+
+    List<Coursetype> selectByExample(CoursetypeExample example);
+
+    Coursetype selectByPrimaryKey(Integer ctid);
+
+    int updateByExampleSelective(@Param("record") Coursetype record, @Param("example") CoursetypeExample example);
+
+    int updateByExample(@Param("record") Coursetype record, @Param("example") CoursetypeExample example);
+
+    int updateByPrimaryKeySelective(Coursetype record);
+
+    int updateByPrimaryKey(Coursetype record);
+}
---
+++ src/com/mooc/dao/SubscribeMapper.java
@@ -0,0 +1,30 @@
+package com.mooc.dao;
+
+import com.mooc.entity.Subscribe;
+import com.mooc.entity.SubscribeExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SubscribeMapper {
+    int countByExample(SubscribeExample example);
+
+    int deleteByExample(SubscribeExample example);
+
+    int deleteByPrimaryKey(Integer sid);
+
+    int insert(Subscribe record);
+
+    int insertSelective(Subscribe record);
+
+    List<Subscribe> selectByExample(SubscribeExample example);
+
+    Subscribe selectByPrimaryKey(Integer sid);
+
+    int updateByExampleSelective(@Param("record") Subscribe record, @Param("example") SubscribeExample example);
+
+    int updateByExample(@Param("record") Subscribe record, @Param("example") SubscribeExample example);
+
+    int updateByPrimaryKeySelective(Subscribe record);
+
+    int updateByPrimaryKey(Subscribe record);
+}
