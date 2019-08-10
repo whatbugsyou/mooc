c9e302a9c7e762b6d91161a9bb52f01ccaed4218
suyinsama <suyin1026@163.com>
2019-08-10 15:54:45 +0800

    课程分类页面能用了，自行添加数据。video页面还没有功能

--- src/com/mooc/dao/CourseMapper.java
+++ src/com/mooc/dao/CourseMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import com.mooc.entity.Course;
+import com.mooc.entity.Coursetype;
 
 @Repository
 public interface CourseMapper {
@@ -13,5 +14,11 @@ public interface CourseMapper {
 	List<Course> selectLikeCname(String name);
 
 	List<Course> selectAll();
+
+	List<Coursetype> selectAllType();
+
+	List<Course> selectCourseByCtid(int ctid);
+
+	List<Course> selectByCplayvolume();
 
 }
